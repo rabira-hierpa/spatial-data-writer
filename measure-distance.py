305de3125605f6b6a66b2f9b4be45f44f2a8e000
Rabra <rabira.hierpa@gmail.com>
2021-03-11 12:29:56 +0300

    tested measure distance

--- measure-distance.py
+++ measure-distance.py
@@ -1,5 +1,6 @@
 # Import necessary modules first
 import geopandas as gpd
+from pyproj import crs
 from shapely.geometry import Point
 
 # Create an empty geopandas GeoDataFrame
@@ -23,10 +24,25 @@ def main():
     gii_towns = read_shape_file(gii_towns_path)
     era_towns = read_shape_file(era_towns_path)
 
-    # print(gii_towns)
-    # print(era_towns['NAME'],era_towns['POINT_X'],era_towns['POINT_Y'])
-    for towns in era_towns.iterrows():
-        print(towns)
+    # for era_town in era_towns.iterrows():
+    #     era_point_town = era_town[1][15][0]
+    #     for gii_town in gii_towns.iterrows():
+    #         gii_point_town = gii_town[1][22]
+    #         points_df = gpd.GeoDataFrame({'geometry': [era_point_town, gii_point_town]}, crs='EPSG:4326')
+    #         points_df2 = points_df.shift() 
+    #         print(points_df.distance(points_df2))
+    point1,point2 = None,None
+    for era_town in era_towns.iterrows():
+        if (era_town[1][2] == 'Mezezo'):
+            era_point = era_town[1][15][0]
+            print(era_town[1][2])
+    for gii_town in gii_towns.iterrows():
+        if (gii_town[1][3] == 'Mezezo'):
+            gii_point = gii_town[1][22]
+
+    points_df = gpd.GeoDataFrame({'geometry':[era_point,gii_point]},crs='EPSG:4326')
+    points_df2 = points_df.shift()
+    print(points_df.distance(points_df2))
 
 if __name__ == '__main__':
     main()
